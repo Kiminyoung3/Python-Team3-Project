fdbc3b14272b5ffb6c05360494ce1e62088e17be
Kiminyoung3 <pie235@naver.com>
2024-07-23 17:12:13 +0900

    Update Project_0723_2-2.py

--- Project_0723_2-2.py
+++ Project_0723_2-2.py
@@ -1,8 +1,4 @@
 class SafetyMetricsCalculator:
-    """
-    Class to calculate safety metrics including Frequency Rate, Severity Rate,
-    Annual Accident Rate, and Comprehensive Disaster Index.
-    """
 
     def 도수율_계산기(self, number_of_accidents, total_work_hours):
         # 도수율 계산하는 함수 = 재해발생건수/근로시간 * 1,000,000
@@ -28,11 +24,7 @@ class SafetyMetricsCalculator:
 
 
 def get_input(prompt):
-    """
-    Get a positive integer input from the user with validation.
-    :param prompt: The prompt message to display to the user.
-    :return: A positive integer input from the user.
-    """
+
     while True:
         try:
             value = int(input(prompt))  # 값을 입력받음
@@ -53,38 +45,53 @@ def main():
 
     while True:
         get_choice = input("계산할 재해 지수를 입력하세요. (도수율/강도율/연천인율/종합재해지수): ").strip()
+        print("-" * 30)
 
         if get_choice == "도수율":
             number_of_accidents = get_input("★ 재해 발생 건수를 입력하세요.: ")
             total_work_hours = get_input("★ 연간 근로시간을 입력하세요.: ")
 
             frequency_rate = calculator.도수율_계산기(number_of_accidents, total_work_hours)
+            print("-" * 10, "계산결과", "-" * 10)
             print("♥ 도수율: ", frequency_rate, "→ 100만 근로시간당", frequency_rate, "건의 재해 발생")
+            print("=" * 30)
 
         elif get_choice == "강도율":
             lost_work_days = get_input("★ 근로손실일수를 입력하세요.: ")
             total_work_hours = get_input("★ 연간 근로시간을 입력하세요.: ")
 
             severity_rate = calculator.강도율_계산기(lost_work_days, total_work_hours)
+            print("-" * 10, "계산결과", "-" * 10)
             print("♥ 강도율: ", severity_rate, "→ 1000시간당", severity_rate, "일의 근로손실일수 발생")
+            print("=" * 30)
 
         elif get_choice == "연천인율":
             number_of_accidents = get_input("★ 재해 발생 건수를 입력하세요.: ")
             total_workers = get_input("★ 연평균 근로자 수를 입력하세요.: ")
 
             annual_accident_rate = calculator.연천인율_계산기(number_of_accidents, total_workers)
+            print("-" * 10, "계산결과", "-" * 10)
             print(f"♥ 연천인율: ", annual_accident_rate, "→ 1년에 1000명 당", annual_accident_rate, "건의 재해 발생")
+            print("=" * 30)
 
         elif get_choice == "종합재해지수":
             while True:
                 disaster = input("도수율과 강도율을 먼저 계산하시겠습니까? (네/아니오): ").strip().lower()
+                print("-" * 30)
                 if disaster == "네":
                     number_of_accidents = get_input("★ 재해 발생 건수를 입력하세요.: ")
                     total_work_hours = get_input("★ 연간 근로시간을 입력하세요.: ")
                     lost_work_days = get_input("★ 근로손실일수를 입력하세요.: ")
 
                     comprehensive_disaster_index1 = calculator.종합재해지수_계산기1(number_of_accidents, total_work_hours, lost_work_days)
+                    frequency_rate1 = calculator.도수율_계산기(number_of_accidents, total_work_hours)
+                    severity_rate1 = calculator.강도율_계산기(lost_work_days, total_work_hours)
+
+                    print("-" * 10, "계산결과", "-" * 10)
+                    print("♥ 도수율: ", frequency_rate1)
+                    print("♥ 강도율: ", severity_rate1)
                     print("♥ 종합재해지수: ", comprehensive_disaster_index1)
+                    print("=" * 30)
                     break
 
                 elif disaster == "아니오":
@@ -92,16 +99,20 @@ def main():
                     gangdo = get_input("★ 강도율을 입력하세요.: ")
 
                     comprehensive_disaster_index2 = calculator.종합재해지수_계산기2(dosu, gangdo)
+                    print("-" * 10, "계산결과", "-" * 10)
                     print("♥ 종합재해지수: ", comprehensive_disaster_index2)
+                    print("=" * 30)
                     break
 
                 else:
                     print("올바른 입력을 하세요. (네/아니오)")
+                    print("=" * 30)
 
         else:
             print("위의 재해 지수 중 한 가지를 입력해주세요.")
 
         repeat = input("계산을 다시 하시겠습니까? (네/아니오): ").strip().lower()
+        print("=" * 30)
         if repeat != '네':
             break
 
